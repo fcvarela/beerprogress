2252565ec03d61b5ba3f21ea68b745cf4c2d6dde
Filipe Varela <fcvarela@gmail.com>
2014-06-25 16:23:39 +0100

    Added description to package

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='beerprogress',
+    description='Command line progress indicator library for Python3',
     version='0.0.1',
     packages=[
         'beerprogress',
